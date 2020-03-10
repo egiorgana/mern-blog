96466c80fcd4ef5e01d6f6fadeb19d88d8e3071d
egiorgana <eagiorganamorales@gmail.com>
2020-03-10 02:28:12 -0600

    login

---
+++ client/src/components/RegisterLogin/index.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { loginUser } from '../../actions/user_actions'
+
+class RegisterLogin extends Component {
+    state = {
+        email: '',
+        password: '',
+        errors: []
+    }
+
+    displayErrors = errors => 
+        errors.map((error, i) => <p key={i}>{error}</p>)
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    submitForm = event => {
+        event.preventDefault()
+        
+        let dataToSubmit = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        if(this.isFormvalid(this.state)){
+            this.setState({ errors: [] })
+                this.props.dispatch(loginUser(dataToSubmit))
+                    .then(response => {
+                        if(response.payload.loginSuccess){
+                            this.props.history.push('/')
+                        }else{
+                            this.setState({
+                                errors: this.state.errors.concat(
+                                    'Failed to log in, you can check your Email and Password'
+                                )
+                            })
+                        }
+                    })
+        }else{
+            this.setState({
+                errors: this.state.errors.concat('Form is not valid')
+            })
+        }
+    }
+
+    isFormvalid = ({ email, password }) => email && password
+
+    render() {
+        return (
+            <div className="container">
+                <h2>Login</h2>
+                <div className="row">
+                    <form className="col s12">
+                        
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input 
+                                    name="email"
+                                    calue={this.state.email}
+                                    onChange={e => this.handleChange(e)}
+                                    id="email"
+                                    type="email"
+                                    className="validate"
+                                />
+                                <label htmlFor="email">Email</label>
+                                <span 
+                                    className="helper-text"
+                                    data-error="Type a right type email" 
+                                    data-success="right"
+                                />
+                            </div>
+                        </div>
+                        
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input 
+                                    name="password"
+                                    calue={this.state.password}
+                                    onChange={e => this.handleChange(e)}
+                                    id="password"
+                                    type="password"
+                                    className="validate"
+                                />
+                                <label htmlFor="email">Password</label>
+                                <span 
+                                    className="helper-text"
+                                    data-error="wrong" 
+                                    data-success="right"
+                                />
+                            </div>
+                        </div>
+
+                        {this.state.errors.length > 0 && (
+                            <div>
+                                {this.displayErrors(this.state.errors)}
+                            </div>
+                        )}
+
+                        <div className="row">
+                            <div className="col 12">
+                                <button 
+                                    className="btn waves-effect red lghten-2"
+                                    type="submit"
+                                    name="action"
+                                    onClick={this.submitForm}
+                                >
+                                    LOGIN
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(RegisterLogin)
---
+++ client/src/components/about/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the shit<span>🤡</span></p>
+            </div>
+        )
+    }
+}
